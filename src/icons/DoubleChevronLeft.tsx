154261dcbb0025dc0adfeaab64c33f09f965e170
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2025-06-20 15:02:48 +0100

    DSP-1257 add 'Pagination' component
    also:
    - refactor 'Icon' component to use icon components generated from DS source using SVGR
    - add said icon components to the repo
    - change any use of old icon names to the new PascalCase names
    - add aria-invalid to form fields when they're in an error state

--- src/icons/DoubleChevronLeft.tsx
+++ src/icons/DoubleChevronLeft.tsx
@@ -2,18 +2,12 @@ import * as React from "react";
 import type { SVGProps } from "react";
 const SvgDoubleChevronLeft = (props: SVGProps<SVGSVGElement>) => (
     <svg
-        id="Layer_1"
         xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        x="0px"
-        y="0px"
         viewBox="0 0 24 24"
-        xmlSpace="preserve"
         role="img"
         {...props}
     >
-        <path d="M18.4,16.6L13.8,12l4.6-4.6L17,6l-6,6l6,6L18.4,16.6z" />
-        <path d="M12.4,16.6L7.8,12l4.6-4.6L11,6l-6,6l6,6L12.4,16.6z" />
+        <path d="M19 16.6 17.6 18l-6-6 6-6L19 7.4 14.4 12l4.6 4.6Zm-6.6 0L11 18l-6-6 6-6 1.4 1.4L7.8 12l4.6 4.6Z" />
     </svg>
 );
 export default SvgDoubleChevronLeft;
